6559733144af46dddb682da6f9a92506630ca9c7
Max Zimmermann <max.zimmermann@greenhouse.io>
2017-12-03 22:24:55 -0500

    Print multiple cards horizontally

--- CardTest.rb
+++ CardTest.rb
@@ -1,7 +1,7 @@
 
 
 def create_deck
-		res = []
+	res = []
 		def make_suite(suite)
 			non_face = [*2..10]
 			
@@ -11,12 +11,43 @@ def create_deck
 			face.map! do |x|
 				x+= suite
 				# below try making [x.to_s] to make an array with x as a string at index 1
+				x = [x, ["|", "\u203E" *8, "|"], ["|#{x}"," "*6, "|"],["|", " "*8, "|"],["|", " "*6, "#{x}|"], ["|", "_"*8, "|"]]
 			  end			
 			face
 		end
+	hearts = make_suite("\u2665")
+	diamonds = make_suite("\u2666")
+	spades = make_suite("\u2660")
+	clubs = make_suite("\u2663")
+	res.concat(hearts)
+	res.concat(diamonds)
+	res.concat(spades)
+	res.concat(clubs)
+	res.shuffle
 end
+
+def print_cards(deck)
+	deck.each {|card| card[1].each {|x| print x }}
+	print "\n"
+	deck.each {|card| card[2].each {|x| print x }}
+	print "\n"
+	deck.each {|card| card[3].each {|x| print x }}
+	print "\n"
+	deck.each {|card| card[4].each {|x| print x }}
+	print "\n"
+	deck.each {|card| card[5].each {|x| print x }}
+	print "\n"
+end
+
 
 card = [["A\u2665"],[["|", "\u203E"*8, "|"], ["|", "A\u2665", " "*6, "|"],["|"," "*8,"|"],["|", " "*6,"A\u2665","|"],
 ["|", "_"*8, "|"]]]
 
 card[1][0].each {|x| print x}
+
+
+
+
+
+
+
